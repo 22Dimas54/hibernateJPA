9d942acf6b8f1912b74be5a6635b7a241a5de290
Dmitry Ushakov <ushakov-dp@mail.ru>
2022-11-29 10:53:31 +0700

    spring_method_security is ready

--- src/main/java/ru/netology/hibernate/SecurityConfiguration.java
+++ src/main/java/ru/netology/hibernate/SecurityConfiguration.java
@@ -1,30 +1,39 @@
 package ru.netology.hibernate;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("read").password("{noop}read").authorities("read")
-                .and()
-                .withUser("write").password("{noop}write").authorities("write");
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("read")
+                .password(encoder().encode("read"))
+                .roles("READ")
                 .and()
-                .authorizeRequests().antMatchers("/hello").permitAll()
+                .withUser("write")
+                .password(encoder().encode("write"))
+                .roles("WRITE")
                 .and()
-                .authorizeRequests().antMatchers("/persons/create", "/persons/update", "/persons/delete").hasAuthority("write")
+                .withUser("delete")
+                .password(encoder().encode("delete"))
+                .roles("DELETE")
                 .and()
-                .authorizeRequests().antMatchers("/persons/by-city", "/persons/readAll",
-                "/persons/find-city", "/persons/find-age", "/persons/find-name-surname").hasAuthority("read")
-                .and()
-                .authorizeRequests().anyRequest().authenticated();
+                .withUser("notRoles")
+                .password("{noop}notRoles")
+                .authorities("read");
     }
 }
--- src/main/java/ru/netology/hibernate/controller/HibernateController.java
+++ src/main/java/ru/netology/hibernate/controller/HibernateController.java
@@ -1,78 +1,43 @@
 package ru.netology.hibernate.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.netology.hibernate.entity.City;
-import ru.netology.hibernate.entity.Persons;
-import ru.netology.hibernate.service.HibernateService;
 
-import java.util.List;
-import java.util.Optional;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 public class HibernateController {
-    @Autowired
-    private HibernateService hibernateService;
-
-    @GetMapping("/persons/by-city")
-    public List<Persons> handlerController(@RequestParam("city") String city) {
-        return hibernateService.handlerService(city);
+    @GetMapping("/read")
+    @Secured({"ROLE_READ"})
+    public String read() {
+        return "read";
     }
 
-    //    CRUD
-    @GetMapping("/persons/create")
-    public Persons createController(@RequestParam("name") String name,
-                                    @RequestParam("surname") String surname,
-                                    @RequestParam("age") int age,
-                                    @RequestParam("phoneNumber") String phoneNumber,
-                                    @RequestParam("city") String city) {
-        return hibernateService.createService(name, surname, age, phoneNumber, city);
+    @GetMapping("/write")
+    @RolesAllowed({"ROLE_WRITE"})
+    public String write() {
+        return "write";
     }
 
-    @GetMapping("/persons/readAll")
-    public List<Persons> readAllController() {
-        return hibernateService.readAllService();
+    @GetMapping("/pre")
+    @PreAuthorize("hasAnyRole('ROLE_WRITE','ROLE_DELETE')")
+    public String pre() {
+        return "pre";
     }
 
-    @GetMapping("/persons/update")
-    public Persons updateController(@RequestParam("name") String name,
-                                    @RequestParam("surname") String surname,
-                                    @RequestParam("age") int age,
-                                    @RequestParam("phoneNumber") String phoneNumber) {
-        return hibernateService.updateService(name, surname, age, phoneNumber);
+    @GetMapping("/post")
+    @PostAuthorize("hasAnyRole('ROLE_WRITE','ROLE_DELETE')")
+    public String post() {
+        return "post";
     }
 
-    @GetMapping("/persons/delete")
-    public String deleteController(@RequestParam("name") String name,
-                                   @RequestParam("surname") String surname,
-                                   @RequestParam("age") int age) {
-        return hibernateService.deleteService(name, surname, age);
-    }
-
-    @GetMapping("/persons/find-city")
-    public City findCityController(@RequestParam("city") String city) {
-        return hibernateService.findCity(city);
-    }
-
-    @GetMapping("/persons/find-age")
-    public List<Persons> findAgeController(@RequestParam("age") int age) {
-        return hibernateService.findAge(age);
-    }
-
-    @GetMapping("/persons/find-name-surname")
-    public Optional<Persons> findByNameSurnameController(@RequestParam("name") String name, @RequestParam("surname") String surname) {
-        return hibernateService.findByNameSurname(name, surname);
-    }
-
-    @GetMapping("/anyRequest")
-    public String anyRequest() {
-        return "any request";
-    }
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello";
+    @GetMapping("/preAuthorize")
+    @PreAuthorize("#username == authentication.principal.username")
+    public String preAuthorize(String username) {
+        return "username: " + username;
     }
 }
